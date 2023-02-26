fcf8e8ca548cfa3ac8eac4da0ce029f26fe54f00
xybai0103 <xueyin0103@gmail.com>
2023-02-26 11:07:23 -0600

    1. add code to get user input from localStorage and set the values of the corresponding textarea elements  2. modify code for event listener on the submit button to save user input in an array and stored in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,16 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  var userInputArray = [];
   $('.saveBtn').click('button', function(event){
     event.preventDefault();
-    // save user input in local storage
+    // save user-input object in an array and stored in local storage
     var userInput = {
       id: $(this).parent().attr('id'),
       input: $(this).prev().val()
     }
-    localStorage.setItem(userInput.id, JSON.stringify(userInput));
+    userInputArray.push(userInput);
+    localStorage.setItem('userInputArray', JSON.stringify(userInputArray));
   });
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -27,8 +29,27 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  
+
+  // get stored user input from local storage
+  storedUserInput = JSON.parse(localStorage.getItem('userInputArray'))
+  for (var i = 0; i < storedUserInput.length; i++) {
+    // $('#'+storedUserInput[i].id) is the containing time-block element with the corresponding id
+    $('#'+storedUserInput[i].id).children('.description').text(storedUserInput[i].input);
+  }
+
+  /* tried way 1 doesn't work
+  var containerId = ["hour-9", "hour-10", "hour-11","hour-12","hour-13","hour-14","hour-15","hour-16","hour-17"];
+  for (var i = 0; i < containerId.length; i++) {
+  $('#'+(containerId[i])).children('.description').text(JSON.parse(localStorage.getItem(containerId[i])).input);
+  };
+  */
+
+  /* tried way 2 doesn't work
+  var containerId = $('.description').parent().attr('id');
+  var storedInput = JSON.parse(localStorage.getItem(containerId)).input;
+  $('.description').text(storedInput);
+  */
+
   // TODO: Add code to display the current date in the header of the page.
   var today = dayjs();
   $('#currentDay').text(today.format('dddd, MMMM D'));
