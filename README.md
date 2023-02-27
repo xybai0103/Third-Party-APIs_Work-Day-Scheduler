3fe18788f1bff92096fa9dc94df371b885b3f65d
xybai0103 <xueyin0103@gmail.com>
2023-02-26 20:06:36 -0600

    move code for displaying header of the page to the top

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # Third-Party-APIs_Work-Day-Scheduler
+
+## Description
+
+This application is 
+Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+You'll need to use the [Day.js](https://day.js.org/en/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Day.js in the browser.
+
+```md
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+## Installation
+
+N/A
+
+## Usage
+
+* W
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+
+## Sreenshot
+
+The following image demonstrates the application appearance:
+
+![ScreenShot](./assets/images/04-webAPIs-codeQuiz-1.png)
+
+## Link to the website
+
+https://xybai0103.github.io/thirdPartyAPIs-workDayScheduler/
+
+## Credits
+
+N/A
+
+## Licencse
+
+N/A
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,11 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  // Display the current date in the header of the page
+  var today = dayjs();
+  $('#currentDay').text(today.format('dddd, MMMM D'));
+
+  // Add a listener for click events on the save button
   var userInputArray = [];
   $('.saveBtn').click('button', function(event){
     event.preventDefault();
@@ -20,13 +18,11 @@ $(function () {
     userInputArray.push(userInput);
     localStorage.setItem('userInputArray', JSON.stringify(userInputArray));
   });
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  // use Day.js to get the current hour in 24-hour time
+
+  // Use Day.js to get the current hour in 24-hour time
   var currentHour = dayjs().format('HH');
+
+  // Apply the past, present, or future class to each time block by comparing the id to the current hour
   var containerId = ["hour-9", "hour-10", "hour-11","hour-12","hour-13","hour-14","hour-15","hour-16","hour-17"];
   for (var i = 0; i < containerId.length; i++) {
     if (i + 9 < currentHour) {
@@ -39,31 +35,23 @@ $(function () {
     }
   }
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-  // get stored user input from local storage
+  // get stored user input from local storage and set the values of the corresponding textarea elements
   storedUserInput = JSON.parse(localStorage.getItem('userInputArray'))
   for (var i = 0; i < storedUserInput.length; i++) {
     // $('#'+storedUserInput[i].id) is the containing time-block element with the corresponding id
     $('#'+storedUserInput[i].id).children('.description').text(storedUserInput[i].input);
   }
 
-  /* tried way 1 doesn't work
+  /* tried way 1 doesn't work, null input
   var containerId = ["hour-9", "hour-10", "hour-11","hour-12","hour-13","hour-14","hour-15","hour-16","hour-17"];
   for (var i = 0; i < containerId.length; i++) {
   $('#'+(containerId[i])).children('.description').text(JSON.parse(localStorage.getItem(containerId[i])).input);
   };
   */
 
-  /* tried way 2 doesn't work
+  /* tried way 2 doesn't work, null input
   var containerId = $('.description').parent().attr('id');
   var storedInput = JSON.parse(localStorage.getItem(containerId)).input;
   $('.description').text(storedInput);
   */
-
-  // TODO: Add code to display the current date in the header of the page.
-  var today = dayjs();
-  $('#currentDay').text(today.format('dddd, MMMM D'));
 });
